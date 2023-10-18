3eb5675c5994250735e84acf453d91fd5220a7c1
burillu <burillu90@hotmail.it>
2023-10-18 16:30:30 +0200

    improve create square width and heigth with js

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,6 +1,5 @@
 
-// costante difficoltà
-const difficulty = 100;
+
 
 //prendo il bottone
 const btnPlay = document.getElementById('play-btn');
@@ -8,26 +7,41 @@ const btnPlay = document.getElementById('play-btn');
 btnPlay.addEventListener('click', createGrid);
 //funzione on click
 function createGrid() {
+    // prendo il valore della select difficoltà
+    const selectDiff= document.getElementById('difficulty');
+    // variabile difficoltà
+    let difficulty;
+    if (selectDiff.value=== 'easy') {
+        difficulty = 100;
+    } else if (selectDiff.value=== 'medium'){
+        difficulty = 81;
+    } else if (selectDiff.value=== 'hard'){
+        difficulty = 49;
+    }
+    console.log(difficulty);
     // prendo il container #playground
     const divPlayground = document.getElementById('playground');
-    divPlayground.innerHTML='';
+    divPlayground.innerHTML = '';
     for (let i = 0; i < difficulty; i++) {
         // creo singolo quadrato
-        createSquare(divPlayground, i);
+        createSquare(divPlayground, i, difficulty);
 
     }
 
 
 }
-function createSquare(divCont, index){
+function createSquare(divCont, index, diff) {
     const square = document.createElement('div');
+    const lineItems = Math.sqrt(diff);
     square.classList.add('box');
-    square.innerText= index + 1;
+    square.style.width= `calc( 100% / ${lineItems})`;
+    square.style.height= `calc( 100% / ${lineItems})`;
+    square.innerText = index + 1;
     square.addEventListener('click', activeBox)
     divCont.append(square);
-    function activeBox (){
+    function activeBox() {
         square.classList.add('active');
-        console.log(`cella attivata: ${this.innerHTML}`);
+        console.log(`attivata cella numero: ${this.innerHTML}`);
     }
 }
 
